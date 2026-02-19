1d39a75ee0724ca16698878eeb7a9c43316dbaa2
adam sinnott <adamsinnott@gmail.com>
2026-02-19 10:53:50 +0000

    Changing API to timeapi as worldtimeapi has been sunsetted.

--- world_date.py
+++ world_date.py
@@ -1,13 +1,12 @@
 from adafruit_datetime import datetime
 
-
-TIME_URL = "https://worldtimeapi.org/api/timezone/Europe/London.json"
+TIME_URL = "https://timeapi.io/api/v1/timezone/zone?timeZone=Europe%2FLondon"
 
 def get_current_date(requests):
   try:
     response = requests.get(str(TIME_URL))
     json = response.json()
-    date = datetime.fromisoformat(json['utc_datetime'])
+    date = datetime.fromisoformat(json['utc_time'])
     date = date.isoformat(sep='T')
     return date
   except Exception as e:
